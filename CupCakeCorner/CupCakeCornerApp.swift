26c4052e4b02aa6ec0854742a5b95670996867a9
shivgirja <amitsrivastava115@yahoo.com>
2021-12-09 16:57:11 +0530

    Initial Commit

---
+++ CupCakeCorner/CupCakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCakeCornerApp.swift
+//  CupCakeCorner
+//
+//  Created by Amit Shrivastava on 09/12/21.
+//
+
+import SwiftUI
+
+@main
+struct CupCakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
